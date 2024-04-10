4724b55db20e47e6d7240429f306a024071c1809
I-sena <isabelasena1297@gmail.com>
2024-04-10 00:03:19 -0300

    Analisador lexico

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from lexical_analyzerCminus import AnalisadorLexico
 
 def main():
-    arquivo = "exemplos/exemplo.cm"
+    arquivo = "exemplos/exemplo3.cm"
 
     analisador_lexico = AnalisadorLexico(arquivo)
     analisador_lexico.printTokens()
